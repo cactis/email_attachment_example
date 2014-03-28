a76b26b2b055b2883d63e818681942f8a34a06d8
cactis <cactis.lin@gmail.com>
2014-03-28 17:57:12 +0800

    added sand mail example

--- Rakefile
+++ Rakefile
@@ -7,11 +7,15 @@ require 'bundler'
 Bundler.require
 require 'motion-prime'
 
+require 'bubble-wrap/mail'
+
 require File.expand_path 'app/environment.rb'
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
-  app.name = 'Prime Project'
+  app.name = 'Mail Test'
 
   app.version = '0.0.1'
   app.icons = %w{Icon.png}
+
+  app.provisioning_profile = '/Users/ctslin/BTSync/_myself/_mac/distribution.mobileprovision'
 end
--- app/screens/home.rb
+++ app/screens/home.rb
@@ -2,6 +2,31 @@ class HomeScreen < Prime::Screen
 
   def render
     set_navigation_right_button "Email" do
+      send_mail
     end
+  end
+
+  def send_mail
+    options = {
+      subject: '== Email Attachment Test ==',
+      message: 'Hi, here is a email attachment test.',
+      html: true
+    }
+
+    mc = MFMailComposeViewController.alloc.init
+    mc.mailComposeDelegate = self
+    mc.setSubject options[:subject]
+    mc.setMessageBody options[:message], isHTML: true
+
+    path = NSBundle.mainBundle.pathForResource('Default-568h@2x', ofType: 'png')
+    attachment = NSData.dataWithContentsOfFile(path)
+
+    mc.addAttachmentData(attachment, mimeType: 'image/png', fileName: 'Default-568h@2x.png')
+
+    self.presentViewController(mc, animated: true, completion: nil)
+  end
+
+  def mailComposeController(controller, didFinishWithResult: result, error: error)
+    self.dismissModalViewControllerAnimated(@mailer_is_animated)
   end
 end
